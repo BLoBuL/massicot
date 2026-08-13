cf95fc8e2796094f5416c1f9e0103bfd17bc0bb6
JuLBLoBuL <54357712+JuLBLoBuL@users.noreply.github.com>
2026-08-13 12:45:20 +0200

    fix: charger le CSS via insert_head_css

--- massicot_pipelines.php
+++ massicot_pipelines.php
@@ -127,6 +127,18 @@ function massicot_formulaire_traiter($flux) {
 }
 
 /**
+ * Charge la feuille du recadreur avec le pipeline CSS natif de SPIP 4.
+ */
+function massicot_insert_head_css($flux) {
+	if (test_espace_prive() && _request('exec') === 'massicoter_image') {
+		include_spip('inc/filtres');
+		$css = timestamp(find_in_path('css/massicot.css'));
+		$flux .= '<link rel="stylesheet" href="' . attribut_html($css) . '" type="text/css">';
+	}
+	return $flux;
+}
+
+/**
  * Ajoute les actions de recadrage au formulaire natif des logos sans
  * surcharger son squelette complet.
  */
